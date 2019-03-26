1610fa309de6b459cdaedf2e281961c665d25042
Jaden Marcus Ren <ruirenjerry@gmail.com>
2019-03-25 22:50:38 -0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+from math import pi
+from math import sin
+from math import cos
+class func:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def Hough_Trans(self):
+        # the distance to the line
+        a = lambda i: self.x * cos(i * pi/180) + self.y * sin(i * pi/180)  
+        distance = [a(i) for i in range(181)]
+        return distance
